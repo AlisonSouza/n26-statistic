b3acb091f0d0db8a7b94fb52c7418d91eaa45409
Alison Rodrigues de Souza <alisons@dbserver.com.br>
2017-07-09 01:05:34 -0300

    Creating the API

---
+++ src/main/java/com/alisonsouza/n26/n26statistic/N26StatisticApplication.java
@@ -0,0 +1,12 @@
+package com.alisonsouza.n26.n26statistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N26StatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N26StatisticApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alisonsouza/n26/n26statistic/N26StatisticApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alisonsouza.n26.n26statistic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class N26StatisticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
